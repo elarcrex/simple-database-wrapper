09cb714d2fadb6c6d14713c9177509127d66e955
elarcrex <nghaka.chawngthu@gmail.com>
2016-06-13 23:31:24 +0530

    Add more examples on index.php

--- index.php
+++ index.php
@@ -5,10 +5,28 @@ use Elarc\Libs\Database;
 use Elarc\Models\Post;
 
 $post = new Post(new Database);
+/* Start */
+var_dump($post->all());
 
-/*$post->title = "First post";
+$post->title = "First post";
 $post->body = "First post body";
 $post->updated_at = date('Y-m-d H:i:s');
-$post->save();*/
+$post->save();
 
 var_dump($post->all());
+/* End */
+
+/* Start */
+$post->id = 5;
+$post->title = "Updated title";
+$post->save();
+
+var_dump($post->all());
+/* End */
+
+/* Start */
+$post->id = 4;
+$post->delete();
+
+var_dump($post->all());
+/* End */
